8e14de4d121d4677391abca5146b7ec99e3edafa
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-11 12:43:08 -0500

    Creación del menú principal

--- index.php
+++ index.php
@@ -1,54 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SITA V2.0</title>
-
-    <link rel="stylesheet" href="./css/bootstrap.min.css"/> <!-- Referencia a la hoja de estilos -->
-
-</head>
-<body>
-    <nav class="navbar navbar-expand navbar-light bg-light"> <!-- Barra de menu -->
-        <ul class="nav navbar-nav">
-            
-            <li class="nav-item">
-                <a class="nav-link" href="#">SITA</a>
-            </li>
-            
-            <li class="nav-item">
-                <a class="nav-link" href="#">Administrador</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Recursos humanos</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Academicos</a>
-            </li>
-
-        </ul>
-    </nav>
-
-    <div class="container"> <!-- Contenido de la pagina variable -->
-        <div class="row">
-
+<?php include("template/cabecera.php"); ?>
 
             <div class="jumbotron">
-                <h1 class="display-3">Jumbo heading</h1>
-                <p class="lead">Jumbo helper text</p>
+                <h1 class="display-3">Bienvenido al Sistema de Trayectorias Academicas</h1>
                 <hr class="my-2">
-                <p>More info</p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
-                </p>
+                <p class="lead">Por favor, seleccione una opcion: </p>
             </div>
 
+<div class="col-md-4"> <!-- divide la pantalla en 3 elementos de 4 espacios de 12, cada uno -->
+<div class="card"> <!-- tarjetas para presentar las secciones -->
+<div class="card-body">
+    <h4 class="card-title">Administrador</h4>
+    <img class="card-img-top" src="img/admin.png" alt="">
+    <a name="" id="" class="btn btn-primary" href="administrador.php" role="button">Entrar</a>
+</div>
+</div>
+</div>
 
-        </div>
-    </div>
+<div class="col-md-4"> <!-- divide la pantalla en 3 elementos de 4 espacios de 12, cada uno -->
+<div class="card"> <!-- tarjetas para presentar las secciones -->
+<div class="card-body">
+    <h4 class="card-title">Recursos Humanos</h4>
+    <img class="card-img-top" src="img/recuhum.png" alt="">
+    <a name="" id="" class="btn btn-primary" href="recursos_humanos.php" role="button">Entrar</a>
+</div>
+</div>
+</div>
 
-</body>
-</html>
+<div class="col-md-4"> <!-- divide la pantalla en 3 elementos de 4 espacios de 12, cada uno -->
+<div class="card"> <!-- tarjetas para presentar las secciones -->
+<div class="card-body">
+    <h4 class="card-title">Academico</h4>
+    <img class="card-img-top" src="img/academ.png" alt="">
+    <a name="" id="" class="btn btn-primary" href="academicos.php" role="button">Entrar</a>
+</div>
+</div>
+</div>
+
+<?php include("template/pie.php"); ?>
---
+++ template/cabecera.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SITA V2.0</title>
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/> <!-- Referencia a la hoja de estilos -->
+
+</head>
+<body>
+    <nav class="navbar navbar-expand navbar-light bg-light"> <!-- Barra de menu -->
+        <ul class="nav navbar-nav">
+            
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">SITA</a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="administrador.php">Administrador</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="recursos_humanos.php">Recursos humanos</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="academicos.php">Academicos</a>
+            </li>
+
+        </ul>
+    </nav>
+
+    <div class="container"> <!-- Contenido de la pagina variable -->
+        <div class="row">
