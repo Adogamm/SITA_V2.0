2fef56b362caf8952561305b053a0b2511836022
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-11 10:50:04 -0500

    Creación de la barra de menú (comentado)

--- index.php
+++ index.php
@@ -6,21 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SITA V2.0</title>
 
-    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/> <!-- Referencia a la hoja de estilos -->
 
 </head>
 <body>
-    SITA Version 2
-
-    <nav class="navbar navbar-expand navbar-light bg-light">
+    <nav class="navbar navbar-expand navbar-light bg-light"> <!-- Barra de menu -->
         <ul class="nav navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
-            </li>
+            
             <li class="nav-item">
-                <a class="nav-link" href="#">Nav 2</a>
+                <a class="nav-link" href="#">SITA</a>
             </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="#">Administrador</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Recursos humanos</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Academicos</a>
+            </li>
+
         </ul>
     </nav>
+
+    <div class="container"> <!-- Contenido de la pagina variable -->
+        <div class="row">
+
+
+            <div class="jumbotron">
+                <h1 class="display-3">Jumbo heading</h1>
+                <p class="lead">Jumbo helper text</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+
 </body>
 </html>
