db4410b59c4606096531d32a493868f56c9fe812
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-11 14:22:29 -0500

    Creación de la interfaz del login

--- administrador/index.php
+++ administrador/index.php
@@ -6,44 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
-  <body>
+    <body>
       
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                
-                <div class="card">
-                    <div class="card-header">
-                        Login
-                    </div>
-                    <div class="card-body">
+        <div class="container">
+            <div class="row">
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-4">
+                <div class="text-center">
+                    </br></br></br></br>
+                    <div class="card">
+                        <div class="card-header">
+                            Login
+                        </div>
+                        <div class="card-body">
+                            <form>
+                                <div class = "form-group">
+                                    <label for="exampleInputEmail1">Usuario</label>
+                                    <input type="text" class="form-control" placeholder="Escribe tu usuario">
+                                </div>
 
-                        <form>
-                        <div class = "form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Contraseña</label>
+                                    <input type="password" class="form-control" placeholder="Escribe tu contraseña">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Acceder</button>
+                            </form>
                         </div>
-                        <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                        </div>
-                        <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Sign In</button>
-                        </form>
-                        
-                        
-
                     </div>
                 </div>
-
             </div>
-            
         </div>
-    </div>
-
-</body>
+    </body>
 </html>
