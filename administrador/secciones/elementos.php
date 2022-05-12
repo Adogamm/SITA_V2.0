f63e7e502889bdff20c96437b82fbdc2d8069f4e
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-12 12:32:22 -0500

    Formato del formulario

--- administrador/secciones/elementos.php
+++ administrador/secciones/elementos.php
@@ -4,6 +4,38 @@
 
     Formulario de agregar academico
 
+    <div class="card">
+        <div class="card-header">
+            Datos de elementos
+        </div>
+        <div class="card-body">
+            
+            <form method="POST" enctype="multipart/form-data">
+
+                <div class = "form-group">
+                    <label for="exampleInputEmail1">ID</label>
+                    <input type="text" class="form-control" name="txtID" placeholder="ID">
+                </div>
+                <div class = "form-group">
+                    <label for="exampleInputEmail1">Nombre</label>
+                    <input type="text" class="form-control" name="txtNombre" placeholder="Nomb">
+                </div>
+                <div class = "form-group">
+                    <label for="exampleInputEmail1">Imagen</label>
+                    <input type="file" class="form-control" name="txtID" placeholder="ID">
+                </div>
+
+                <div class="btn-group" role="group" aria-label="">
+                    <button type="button" class="btn btn-success">Agregar</button>
+                    <button type="button" class="btn btn-warning">Modificar</button>
+                    <button type="button" class="btn btn-info">Cancelar</button>
+                </div>
+
+            </form>
+        
+        </div>
+    </div>
+
 </div>
 
 <div class="col-md-7">
