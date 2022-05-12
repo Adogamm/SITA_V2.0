dbec1d39175bb0392145da62389fa30f9da0353b
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-12 13:06:59 -0500

    Subir información con el método POST

--- administrador/secciones/elementos.php
+++ administrador/secciones/elementos.php
@@ -1,4 +1,8 @@
 <?php include("../template/cabecera.php"); ?>
+<?php
+print_r($_POST); //<!-- recepciona texto -->
+print_r($_FILES); //<!-- recepciona archivos -->
+?>
 
 <div class="col-md-5">
 
@@ -14,21 +18,21 @@
 
                 <div class = "form-group">
                     <label for="exampleInputEmail1">ID</label>
-                    <input type="text" class="form-control" name="txtID" placeholder="ID">
+                    <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
                 </div>
                 <div class = "form-group">
                     <label for="exampleInputEmail1">Nombre</label>
-                    <input type="text" class="form-control" name="txtNombre" placeholder="Nomb">
+                    <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nomb">
                 </div>
                 <div class = "form-group">
                     <label for="exampleInputEmail1">Imagen</label>
-                    <input type="file" class="form-control" name="txtID" placeholder="ID">
+                    <input type="file" class="form-control" name="txtImagen" id="txtImagen" placeholder="imagen">
                 </div>
 
                 <div class="btn-group" role="group" aria-label="">
-                    <button type="button" class="btn btn-success">Agregar</button>
-                    <button type="button" class="btn btn-warning">Modificar</button>
-                    <button type="button" class="btn btn-info">Cancelar</button>
+                    <button type="submit" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
+                    <button type="submit" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
+                    <button type="submit" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
                 </div>
 
             </form>
