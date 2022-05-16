f4ecb659e10623e455a04839b4abbb7edfcd1ced
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-16 15:59:44 -0500

    Integrando el buscador

--- administrador/template/tablaAcademicos.php
+++ administrador/template/tablaAcademicos.php
@@ -1,4 +1,11 @@
 <!-- Tabla de academicos (Muestra los datos de los academicos) -->
+<form action="" method="get">
+    <input type="text" name="busqueda">
+    <input type="submit" name="enviar" value="buscar">
+</form>
+
+</br>
+
 <table class="table table-bordered">
     <thead>
         <tr>
@@ -11,22 +18,28 @@
     <tbody>
         <?php foreach($listaElementos as $elementos){?>
         <tr>
-            <td><?php echo $elementos['id']; ?></td>
-            <td><?php echo $elementos['nombre']; ?></td>
-            <td>
-                <div align="center">
-                    <img class="img-thumbnail rounded" src="../../img/<?php echo $elementos['imagen']; ?>" width="100" alt="" srcset="">
-                </div>
-            </td>
-            <td>
-                Seleccionar | Borrar
-                <form method="post">
-                    <input type="hidden" name="txtID" value="<?php echo $elementos['id']; ?>" />
-                    <input type="submit" name="accion" value="Seleccionar" class="btn btn-primary"/>
-                    <input type="submit" name="accion" value="Borrar" class="btn btn-danger"/>
-                </form>
-            </td>
-        </tr>
+        
+        <?php
+            if(isset($_GET['enviar'])) {
+                $busqueda = $_GET['busqueda'];
+                $sentenciaSQL= $conexion->query("SELECT * FROM elementos WHERE nombre LIKE '%$busqueda%'");
+                while ($row = $sentenciaSQL->fetch(PDO::FETCH_LAZY)) { ?>
+                    <td><?php echo $elementos['id']; ?></td>
+                    <td><?php echo $elementos['nombre']; ?></td>
+                    <td>
+                        <div align="center">
+                            <img class="img-thumbnail rounded" src="../../img/<?php echo $elementos['imagen']; ?>" width="100" alt="" srcset="">
+                        </div>
+                    </td>
+                    <td>
+                        <form method="post">
+                            <input type="hidden" name="txtID" value="<?php echo $elementos['id']; ?>" />
+                            <input type="submit" name="accion" value="Seleccionar" class="btn btn-primary"/>
+                            <input type="submit" name="accion" value="Borrar" class="btn btn-danger"/>
+                        </form>
+                    </td>
+                    <?php } ?>
+            <?php } ?>
         <?php } ?>
     </tbody>
 </table>
