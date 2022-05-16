28a97932a1b3e0e0177e0f7749be3d28637cf120
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-05-16 12:59:05 -0500

    Separar la tabla a un template

--- administrador/secciones/editElementos.php
+++ administrador/secciones/editElementos.php
@@ -159,51 +159,7 @@ $listaElementos=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 </div>
 
 <div class="col-md-7">
-
-    <!-- Tabla de academicos (Muestra los datos de los academicos) -->
-
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Imagen</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($listaElementos as $elementos){?>
-            <tr>
-                <td><?php echo $elementos['id']; ?></td>
-                <td><?php echo $elementos['nombre']; ?></td>
-                <td>
-                    
-                    <div align="center">
-                        <img class="img-thumbnail rounded" src="../../img/<?php echo $elementos['imagen']; ?>" width="100" alt="" srcset="">
-                    </div>
-                </td>
-                
-                <td>
-
-                    Seleccionar | Borrar
-                
-                    <form method="post">
-
-                        <input type="hidden" name="txtID" value="<?php echo $elementos['id']; ?>" />
-
-                        <input type="submit" name="accion" value="Seleccionar" class="btn btn-primary"/>
-
-                        <input type="submit" name="accion" value="Borrar" class="btn btn-danger"/>
-                    
-                    </form>
-
-                </td>
-
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-
+<?php include("../template/tablaAcademicos.php"); ?> <!-- Tabla que muestra los academicos registrados -->
 </div>
 
 <?php include("../template/pie.php"); ?>
